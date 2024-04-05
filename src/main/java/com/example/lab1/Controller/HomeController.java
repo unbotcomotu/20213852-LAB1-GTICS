0f8bb929d7ca56cbe4e6c027526490213a9f632e
Josh <a20213852@pucp.edu.pe>
2024-04-04 21:56:20 -0500

    cambios hechos en branch logo

---
+++ src/main/java/com/example/lab1/Controller/HomeController.java
@@ -0,0 +1,19 @@
+package com.example.lab1.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/principal")
+    public String vistaPrincipal(@RequestParam(value = "code",required = false) String code){
+        if(code.equals("PUCP")){
+            return "vistaPrincipal2";
+        }else{
+            return "vistaPrincipal";
+        }
+
+    }
+}
